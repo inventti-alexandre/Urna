c42fe624ce3ae389613065185d6e8442b2b50507
infoA29 <infoA29@INFO07>
2018-10-02 08:54:35 -0300

    Dto cadastro de eleitor incompleto

---
+++ Frei.Marcos.TRE/Frei.Marcos.TRE/DB/DTO_CadastroEleitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frei.Marcos.TRE.DB
+{
+    class DTO_CadastroEleitor
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public DateTime Nascimento{ get; set; }
+        public string Inscricao { get; set; }
+        public int Zona { get; set; }
+        public int Secao{ get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+
+    }
+}
